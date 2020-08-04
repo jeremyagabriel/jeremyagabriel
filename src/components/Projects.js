516e5427cdad0b94f562e1932cacf6f7aa4b0304
Jeremy Gabriel <jeremyagabriel@gmail.com>
2020-08-03 22:05:17 -0700

    Added happy trails mobile preview in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import BattleshipGIF from '../images/battleshipgif.gif'
 import CatTinderGIF from '../images/cat-tinder-gif.gif'
 import HappyTrailsGIF from '../images/happy-trails-preview.gif'
 import AptFinder from '../images/micasa-preview-gif.gif'
+import HappyTrailsMobile from '../images/happy-trails-mobile-preview.jpg'
 import DogTransparent from '../images/dog-transparent.png'
 import { NavLink, Link } from 'react-router-dom'
 import '../styles/Projects.css';
@@ -95,6 +96,22 @@ const Projects = props => {
               <span className="tag first">React</span>
               <span className="tag">CSS</span>
               <span className="tag">HTML</span>
+            </div>
+          </div>
+        </div>
+        <div className="project">
+        <a href="https://github.com/jeremyagabriel/happytrails-react-native-redux" className="projectLink" target="_blank" rel="noopener noreferrer">
+          <img className="projectImage" alt="portfolio-project-repo" src={HappyTrailsMobile} />
+        </a>
+          <div className="projectCaptionContainer">
+            <a className="projectTitleLink" href="https://github.com/jeremyagabriel/happytrails-react-native-redux" target="_blank" rel="noopener noreferrer"><h6 className="projectTitle">Happy Trails Mobile</h6></a>
+            <p className="projectCaption">Porting my Happy Trails web app into mobile is a work in progress! Built in React Native, this project will also utilize Node.js, Express, and MongoDB. State management is built with Redux. <a className="repo-link" href="https://github.com/jeremyagabriel/happytrails-react-native-redux" target="_blank" rel="noopener noreferrer">View repo</a></p>
+            <div className="tags-wrapper">
+              <span className="tag first">React Native</span>
+              <span className="tag">Redux</span>
+              <span className="tag">Node.js</span>
+              <span className="tag">Express</span>
+              <span className="tag last">MongoDB</span>
             </div>
           </div>
         </div>

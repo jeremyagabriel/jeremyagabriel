0c98c12c619ea7cae70d18799bf228aa6e364701
jeremyagabriel <jeremyagabriel@gmail.com>
2021-02-19 10:52:15 -0800

    added new site redirect

--- src/images/node_modules/gatsby-plugin-facebook-pixel/src/gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-exports.onRouteUpdate = function () {
-  // Don't track while developing.
-  if (process.env.NODE_ENV === `production` && typeof fbq === `function`) {
-    fbq("track", "ViewContent");
-  }
-};
